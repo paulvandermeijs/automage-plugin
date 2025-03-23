5c4ae19b44fcada773c31ad55efeff3251108db0
Paul van der Meijs <vandermeijs@redkiwi.nl>
2025-03-23 09:16:22 +0100

    Set namespace

--- Plugin/{{ plugin_name }}.php
+++ Plugin/{{ plugin_name }}.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 namespace {{ namespace }}\Plugin;
+
 class {{ plugin_name }}
 {
-    public function {{ plugin_type }}{{ plugin_method }}({{ subject_name }} $subject)
+    public function {{ plugin_type }}{{ plugin_method }}(\{{ subject_name }} $subject, ...$args)
     {
         // Plugin code...
     }
